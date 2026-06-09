5318cc69392d6b4717ec82a52e399fbd3ac5c3cf
Michael <101007925+Xpos587@users.noreply.github.com>
2026-06-09 11:17:29 +0200

    fix(lint): sort imports in MainActivity.kt

--- mobile/androidApp/src/main/java/ru/skatelab/capture/MainActivity.kt
+++ mobile/androidApp/src/main/java/ru/skatelab/capture/MainActivity.kt
@@ -29,8 +29,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import dagger.hilt.android.AndroidEntryPoint
 import ru.skatelab.capture.presentation.navigation.AppNavigation
